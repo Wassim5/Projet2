0425c8e67ff19f69f8436ca4f8944e8a67caf536
Wassim5 <37782298+Wassim5@users.noreply.github.com>
2018-05-29 23:31:40 +0200

    Update exp_by_squaring_iterative.py

--- Expo by Squaring/exp_by_squaring_iterative.py
+++ Expo by Squaring/exp_by_squaring_iterative.py
@@ -30,5 +30,29 @@ def expo(x, n):
 
 return r,s
 
-def minProd(k)
-    
+# De la meme façon, on met on place une fonction, qui retourne le numéro d'itération pour l'expo x^k
+def minProd(k):
+   # si n=k alors 0 itérations  
+   if k==1 :
+        s=0
+   # si k=2 alors il faut 1 itération, elle suffit      
+    elif k==2:
+        s=1
+    # sinon 
+    else:
+        s=2 
+        r=3
+        while(r<k):
+            s=s+1
+            r=2*r
+        s==s+1
+        #retourne la valeur de s à la fin, s indique le nombre d'itération (minimales) pour arriver à x^k  
+    return s
+
+# Maintenant on calcule " la somme k allant de 1 à 200 de minProd(k)"
+s=0
+for k in range (200):
+    a=minprod(k)
+    s=s+a
+   #Afficher le résultat final
+print(s)
